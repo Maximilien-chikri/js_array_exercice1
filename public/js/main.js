d29d79671b932d73e2ad02212fa2492a774514b1
Maximilien <maximilienchikri@gmail.com>
2020-12-30 14:47:00 +0100

    fin exo

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+// 1
+let classe = ["coding 15" , "coding 16" , "coding 17"]
+
+// 2
+console.log(classe);
+
+// 3
+console.log(classe.length);
+
+// 4
+console.log(classe[0]);
+console.log(classe[1]);
+console.log(classe[2]);
+
+
